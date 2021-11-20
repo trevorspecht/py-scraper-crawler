76b8acfafbcddc1343d246a8764d8b1ac331ddbf
trevorspecht <trevor.specht@gmail.com>
2021-11-20 17:11:16 -0500

    step 1 working

--- irs_forms/irs_forms/pipelines.py
+++ irs_forms/irs_forms/pipelines.py
@@ -6,12 +6,37 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import json
 
 
 class IrsFormsPipeline:
-    formArray = [{}]
-    def process_item(self, item, spider):
-        self.formArray[0] = item
+    form_array = []
 
+    def open_spider(self, spider):
+        self.file = open('forms.json', 'w')
+    
     def close_spider(self, spider):
-        print('result array: ', self.formArray)
+        formatted_array = json.dumps(self.form_array, indent=4)
+        self.file.write(formatted_array)
+        self.file.close()
+
+    def process_item(self, item, spider):
+
+        f = (form for index,form in enumerate(self.form_array) if form.get('form_number') == item['form_number'])
+        i = (index for index,form in enumerate(self.form_array) if form.get('form_number') == item['form_number'])
+        form = next(f, None)
+        index = next(i, None)
+        if form:
+            if item['min_year'] < form['min_year']:
+                form['min_year'] = item['min_year']
+            if item['max_year'] > form['max_year']:
+                form['max_year'] = item['max_year']
+
+            self.form_array.pop(index)
+            self.form_array.insert(index, form)
+        else:
+            self.form_array.append(item)
+
+        return item
+        
+
--- irs_forms/irs_forms/spiders/irsforms.py
+++ irs_forms/irs_forms/spiders/irsforms.py
@@ -3,55 +3,40 @@ import scrapy
 
 class IrsformsSpider(scrapy.Spider):
     name = 'irsforms'
-    start_urls = ['https://apps.irs.gov/app/picklist/list/priorFormPublication.html/']
-    min_year = 3000
-    max_year = 0
-    page_count = 0
+    form_list = ['1099-A', '1095-C', '706-NA']
+    result = []
+
 
     def start_requests(self):
-        urls = {
-            # 'W-2': 'https://apps.irs.gov/app/picklist/list/priorFormPublication.html?value=Form+W-2&criteria=formNumber',
-            '1040': 'https://apps.irs.gov/app/picklist/list/priorFormPublication.html?value=Form+1040&criteria=formNumber'
-        }
+        for form in self.form_list:
+            url = 'https://apps.irs.gov/app/picklist/list/priorFormPublication.html?value=Form+{}&criteria=formNumber'.format(form)
+            yield scrapy.Request(url=url, callback=self.parse, cb_kwargs=dict(form=form))
 
-        for name, url in urls.items():
-            yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response):
+    def parse(self, response, form):
         table_rows = response.xpath('//table[@class="picklist-dataTable"]/tr')
-
-        for row in table_rows:
-            form_number = row.xpath('./td[has-class("LeftCellSpacer")]/a/text()').get()
-            year = row.xpath('normalize-space(./td[@class="EndCellSpacer"]/text())').get()
-
-            if year:
-                year_int = int(year)
-                if year_int < self.min_year:
-                    self.min_year = year_int
-                if year_int > self.max_year:
-                    self.max_year = year_int
-
-            if form_number == 'Form 1040':
-                yield {
-                    # 'form_link': response.xpath('//td[has-class("LeftCellSpacer")]/a/@href').get(),
-                    'form_number': form_number,
-                    'form_title': row.xpath('normalize-space(./td[@class="MiddleCellSpacer"]/text())').get(),
-                    'min_year': self.min_year,
-                    'max_year': self.max_year
-                }
-
         next_page_link = response.xpath('//div[@class="paginationBottom"]/a[contains(text(), "Next")]/@href').get()
         next_page_absolute_url = response.urljoin(next_page_link)
 
-        if self.page_count < 5:
-            self.page_count += 1
-            yield scrapy.Request(url=next_page_absolute_url, callback=self.parse)
+        for row in table_rows:
+            form_number = row.xpath('./td[@class="LeftCellSpacer"]/a/text()').get()
 
+            if form_number == 'Form {}'.format(form):
+                form_title = row.xpath('normalize-space(./td[@class="MiddleCellSpacer"]/text())').get()
+                year = row.xpath('normalize-space(./td[@class="EndCellSpacer"]/text())').get()
 
+                form_entry = {
+                    # 'form_link': response.xpath('//td[has-class("LeftCellSpacer")]/a/@href').get(),
+                    'form_number': form_number,
+                    'form_title': form_title,
+                    'min_year': year,
+                    'max_year': year
+                }
 
-# "form_number": "Form W-2",
-# "form_title": "Wage and Tax Statement (Info Copy Only)",
-# "min_year": 1954,
-# "max_year": 2021
+                yield form_entry
+        
+        if next_page_link:
+            yield scrapy.Request(url=next_page_absolute_url, callback=self.parse, cb_kwargs=dict(form=form))
+
 
 
